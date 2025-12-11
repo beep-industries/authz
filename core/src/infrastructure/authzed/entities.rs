7e02076bb280dc20be7bebb4a099c3d01928d7c8
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-12-11 11:55:23 +0100

    feat: basic sdks
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- core/src/infrastructure/authzed/entities.rs
+++ core/src/infrastructure/authzed/entities.rs
@@ -1,3 +1,5 @@
+use crate::authzed::api::v1::{Relationship, RelationshipUpdate};
+
 pub enum Operation {
     Create,
     Delete,
@@ -12,5 +14,41 @@ impl Into<i32> for Operation {
             Operation::Touch => 2,
             Operation::Delete => 3,
         }
+    }
+}
+
+
+impl Into<RelationshipUpdate> for Relationship {
+    fn into(self) -> RelationshipUpdate {
+        RelationshipUpdate {
+            operation: 0,
+            relationship: Some(self),
+        }
+    }
+}
+
+pub trait Action {
+    fn delete(&self) -> RelationshipUpdate;
+    fn create(&self) -> RelationshipUpdate;
+    fn touch(&self) -> RelationshipUpdate;
+}
+
+impl<T: Into<Relationship> + Clone> Action for T {
+    fn delete(&self) -> RelationshipUpdate {
+        let mut relationship_update: RelationshipUpdate = Into::<Relationship>::into(self.clone()).into();
+        relationship_update.operation = Operation::Delete.into(); 
+        relationship_update
+    }
+
+    fn create(&self) -> RelationshipUpdate {
+        let mut relationship_update: RelationshipUpdate = Into::<Relationship>::into(self.clone()).into();
+        relationship_update.operation = Operation::Delete.into(); 
+        relationship_update
+    }
+
+    fn touch(&self) -> RelationshipUpdate {
+        let mut relationship_update: RelationshipUpdate = Into::<Relationship>::into(self.clone()).into();
+        relationship_update.operation = Operation::Touch.into(); 
+        relationship_update
     }
 }
