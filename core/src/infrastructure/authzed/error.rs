07d27b50b836ae68b3bfe0d86eba1c36e569be13
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-12-11 14:27:10 +0100

    feat(authzed): client add filtered delete
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- core/src/infrastructure/authzed/error.rs
+++ core/src/infrastructure/authzed/error.rs
@@ -5,7 +5,12 @@ pub enum AuthzedError {
     #[error("Could not connect to spicedb: {msg}")]
     ConnectionError { msg: String },
 
+    #[error("Could not write relationship: {msg}")]
+    WriteRelationshipError { msg: String },
 
-    #[error("Could not put relationship: {msg}")]
-    PutRelationshipError { msg: String },
+    #[error("Could not write relationships: {msg}")]
+    WriteRelationshipsError { msg: String },
+
+    #[error("Could not delete relationship: {msg}")]
+    DeleteRelationshipError { msg: String },
 }
