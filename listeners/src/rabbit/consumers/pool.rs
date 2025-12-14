4d3d8d83023fc3ebd2608d41d9e1f854e89337c2
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-12-14 12:24:27 +0100

    refacto: allow consumer handler to be async
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- listeners/src/rabbit/consumers/pool.rs
+++ listeners/src/rabbit/consumers/pool.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-};
+use std::{collections::HashMap, marker::PhantomData, sync::Arc};
 
 use prost::Message;
 use tokio::task::JoinSet;
@@ -17,23 +14,26 @@ where
 }
 
 // Concrete implementation for a typed consumer
-struct TypedConsumerSpawner<S, M>
+struct TypedConsumerSpawner<S, M, H>
 where
     S: Send + Sync + 'static,
     M: Message + Default + 'static,
+    H: MessageHandler<Arc<S>, M> + Clone + 'static,
 {
     queue_name: String,
-    handler: MessageHandler<Arc<S>, M>,
+    handler: H,
+    _phantom: PhantomData<(S, M)>,
 }
 
-impl<S, M> ConsumerSpawner<S> for TypedConsumerSpawner<S, M>
+impl<S, M, H> ConsumerSpawner<S> for TypedConsumerSpawner<S, M, H>
 where
     S: Send + Sync + 'static,
     M: Message + Default + 'static,
+    H: MessageHandler<Arc<S>, M> + Clone + 'static,
 {
     fn spawn(&self, lapin: Arc<RabbitClient>, state: Arc<S>, join_set: &mut JoinSet<()>) {
         let queue_name = self.queue_name.clone();
-        let handler = self.handler;
+        let handler = self.handler.clone();
 
         join_set.spawn(async move {
             let _ = lapin.consume_messages(state, queue_name, handler).await;
@@ -41,23 +41,96 @@ where
     }
 }
 
+// Struct to hold consumers independently from RabbitClient
+pub struct Consumers<S>
+where
+    S: Send + Sync + 'static,
+{
+    spawners: HashMap<String, Box<dyn ConsumerSpawner<S>>>,
+}
+
+impl<S> Consumers<S>
+where
+    S: Send + Sync + 'static,
+{
+    pub fn new() -> Self {
+        Self {
+            spawners: HashMap::new(),
+        }
+    }
+
+    pub fn add<M, H>(mut self, queue_name: &str, message_handler: H) -> Self
+    where
+        M: Message + Default + 'static,
+        H: MessageHandler<Arc<S>, M> + Clone + 'static,
+    {
+        let spawner = TypedConsumerSpawner {
+            queue_name: queue_name.to_string(),
+            handler: message_handler,
+            _phantom: PhantomData,
+        };
+
+        self.spawners
+            .insert(queue_name.to_string(), Box::new(spawner));
+
+        self
+    }
+
+    pub fn merge(mut self, other: Consumers<S>) -> Self {
+        for (queue_name, spawner) in other.spawners.into_iter() {
+            // Only insert if we don't already have this queue
+            self.spawners.entry(queue_name).or_insert(spawner);
+        }
+        self
+    }
+
+    pub fn has_consumer(&self, queue_name: &str) -> bool {
+        self.spawners.contains_key(queue_name)
+    }
+
+    pub fn count(&self) -> usize {
+        self.spawners.len()
+    }
+
+    // Spawn all consumers
+    pub(crate) fn spawn_all(
+        self,
+        lapin: Arc<RabbitClient>,
+        state: Arc<S>,
+        join_set: &mut JoinSet<()>,
+    ) {
+        for (_, spawner) in self.spawners.into_iter() {
+            spawner.spawn(Arc::clone(&lapin), Arc::clone(&state), join_set);
+        }
+    }
+}
+
+impl<S> Default for Consumers<S>
+where
+    S: Send + Sync + 'static,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 pub struct ConsumerPool<S>
 where
-    S: Send + Sync,
+    S: Send + Sync + 'static,
 {
     lapin: Arc<RabbitClient>,
     state: Arc<S>,
-    consumers: Arc<RwLock<HashMap<String, Box<dyn ConsumerSpawner<S>>>>>,
+    consumers: Consumers<S>,
 }
 
 impl<S> ConsumerPool<S>
 where
     S: Send + Sync + 'static,
 {
-    pub fn new(lapin: RabbitClient, state: S) -> Self {
+    pub fn new(lapin: RabbitClient, state: S, consumers: Consumers<S>) -> Self {
         let lapin = Arc::new(lapin);
         let state = Arc::new(state);
-        let consumers = Arc::new(RwLock::new(HashMap::new()));
+
         Self {
             lapin,
             state,
@@ -65,76 +138,20 @@ where
         }
     }
 
-    pub fn consume<M>(self, queue_name: String, message_handler: MessageHandler<Arc<S>, M>) -> Self
-    where
-        M: Message + Default + 'static,
-    {
-        let spawner = TypedConsumerSpawner {
-            queue_name: queue_name.clone(),
-            handler: message_handler,
-        };
-
-        let mut consumers = self.consumers.write().expect("Failed to lock consumers");
-
-        consumers.insert(queue_name, Box::new(spawner));
-
-        drop(consumers);
-        self
-    }
-
-    pub fn merge(self, other: ConsumerPool<S>) -> Self {
-        // Extract other's consumers
-        let other_consumers = Arc::try_unwrap(other.consumers)
-            .unwrap_or_else(|_| panic!("Other consumers still has multiple references"))
-            .into_inner()
-            .expect("Lock poisoned");
-
-        let mut self_consumers = self
-            .consumers
-            .write()
-            .expect("Failed to lock self consumers");
-
-        // Move all consumers from other into self
-        for (queue_name, spawner) in other_consumers.into_iter() {
-            // Only insert if we don't already have this queue
-            self_consumers.entry(queue_name).or_insert(spawner);
-        }
-
-        drop(self_consumers);
-
-        self
-    }
-
     pub fn is_consuming(&self, queue_name: &str) -> bool {
-        self.consumers
-            .read()
-            .expect("Failed to lock consumers")
-            .contains_key(queue_name)
+        self.consumers.has_consumer(queue_name)
     }
 
     pub fn active_queue_count(&self) -> usize {
-        self.consumers
-            .read()
-            .expect("Failed to lock consumers")
-            .len()
+        self.consumers.count()
     }
 
     pub async fn start(self) {
         let mut join_set = JoinSet::new();
 
-        let consumers = Arc::try_unwrap(self.consumers)
-            .unwrap_or_else(|_| panic!("Consumers still has multiple references"))
-            .into_inner()
-            .expect("Lock poisoned");
-
-        // Spawn all consumers
-        for (_, spawner) in consumers.into_iter() {
-            spawner.spawn(
-                Arc::clone(&self.lapin),
-                Arc::clone(&self.state),
-                &mut join_set,
-            );
-        }
+        // Spawn all consumers using the Consumers method
+        self.consumers
+            .spawn_all(self.lapin, self.state, &mut join_set);
 
         // Wait for all tasks to complete
         join_set.join_all().await;
